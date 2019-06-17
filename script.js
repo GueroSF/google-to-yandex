d3456c96e5e8abd2b4556f4e8a926ffc642ad7aa
GueroSF <guerosf@yandex.ru>
2019-06-17 19:03:54 +0300

    small change

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const data = [];
-let artist, song;
-document.querySelectorAll('.song-row').forEach(value => {
-    artist = value.querySelector('[data-col="artist"] a').innerHTML;
-    song = value.querySelector('[data-col="title"] .tooltip').innerHTML.replace(/(\<img .*\>)/, '');
-    data.push(artist + ', ' + song);
-  }
-); 
-console.log(data.join('\n'));
+(() => {
+    const data = [];
+    let artist, song;
+    document.querySelectorAll('.song-row').forEach(value => {
+        artist = value.querySelector('[data-col="artist"] a').innerHTML;
+        song = value.querySelector('[data-col="title"] .tooltip').innerHTML.replace(/(\<img .*\>)/, '');
+        data.push(artist + ', ' + song);
+      }
+    ); 
+    console.log(data.join('\n'));
+})();
